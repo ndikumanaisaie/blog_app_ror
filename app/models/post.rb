280cf8e3330ffef11a752ac098d949c6fa1f3dc5
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-02-22 13:15:41 +0200

    Change user_id to author_id for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 end
