b1662cdc5209ac3fbb167bf7c4a19c997f3c52a5
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-02-22 14:15:12 +0200

    Add custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  after_save :update_comments_counter
+
+  def update_comments_counter
+    post.increment!(:posts_counter)
+  end
 end
