8e8d85387665a41f4594f7673e777e3c50c2f6cb
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-02-23 21:05:29 +0200

    Add test for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  #tests go here
+  describe 'Validations for the comment model' do
+    before(:each) do
+      @comment = Comment.new(text: 'First comment', author_id: 2, post_id: 1)
+    end
+
+    before { @comment.save }
+
+    it 'if author_id is present' do
+      @comment.text = nil
+      expect(@comment).to_not be_valid
+    end
+
+    it 'if author_id is present' do
+      @comment.author_id = 'a'
+      expect(@comment).to_not be_valid
+    end
+    
+    it 'if post_id is present' do
+      @comment.post_id = nil
+      expect(@comment).to_not be_valid
+    end
+
+  end
 end
