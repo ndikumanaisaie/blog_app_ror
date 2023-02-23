054cd7dd43a20e5b1244c7018d8aa046c9cdbc74
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-02-23 20:11:10 +0200

    Add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, through: :posts
   has_many :likes
 
+  validates :name, presence: true
+  validates :post_counter presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
   def recent_posts
     posts.last(3)
   end
