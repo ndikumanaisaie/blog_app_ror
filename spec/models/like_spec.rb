0a0c1484d08b30a9505240b8bcdd73b2ef5b8942
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-02-23 20:57:57 +0200

    Test like model

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  describe 'Validations for the Like model' do
+    before(:each) do
+      @like = Like.new(author_id: 4, post_id: 5)
+    end
+
+    before { @like.save }
+
+    it 'if author_id is present' do
+      @like.author_id = 'a'
+      expect(@like).to_not be_valid
+    end
+    
+    it 'if post_id is present' do
+      @like.post_id = nil
+      expect(@like).to_not be_valid
+    end
+
+  end
+end
