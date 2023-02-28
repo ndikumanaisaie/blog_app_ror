b25ca808382bdfc39dbc95c8143a77f8817a93f0
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-02-28 23:40:53 +0200

    Fix migration error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_22_112653) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_213433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,8 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_112653) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
+    t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
@@ -27,8 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_112653) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
+    t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
