8e4075b76a51c43ba36420f527b51cabc20d7b91
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-02-28 06:44:15 +0200

    Create routes for each URL

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "users#index"
+
+  resources: users only: [:index, :show] do 
+    resources: posts only: [:index, :new, :show] do
+      resources :likes
+      resources :comments 
+    end
+  end
 end
