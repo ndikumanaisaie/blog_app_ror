4abb10d7864acc80ec3d3c003d06d9e264d81b03
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-02-28 10:12:42 +0200

    Add like and comments controller logic

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,23 @@
 class CommentsController < ApplicationController
-  def new; end
+  def new
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
 
-  def create; end
+  def create
+    @comment = Comment.new(post_id: params[:id], author_id: current_user.id, text: params[:text])
+    if @comment.save
+      @comment.update_comments_counter
+    else
+      flash[:error] = 'There is an error'
+    end
+    redirect_to user_post_path(params[:user_id], params[:id])
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.update_comments_counter
+    @comment.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,12 @@
 class LikesController < ApplicationController
-  def new; end
-
-  def create; end
+  def create
+    @like = Like.new(post_id: params[:id], author_id: current_user.id)
+    @post = Post.find(params[:post_id])
+    if @like.save
+      redirect_to user_post_path(params[:user_id], params[:id])
+    else
+      flash[:error] = @like.errors.full_messages.join(', ')
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
