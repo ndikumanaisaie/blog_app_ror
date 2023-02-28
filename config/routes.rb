581135cf346f435559774a8adcd114b62d9427a2
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-02-28 09:20:20 +0200

    Add proper routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :new, :show] do
-      resources :comments, only: [:new, :create, :destroy]
-      resources :likes, only: [:new, :create]
-    end
+    resources :posts, only: [:index, :show]
   end
 end
