42b9121c56d439cb301a5d2305a904c1c35e207c
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-03-03 00:38:36 +0200

    Add styles and path

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,9 +3,9 @@ class LikesController < ApplicationController
     @like = Like.new(post_id: params[:id], author_id: current_user.id)
     @post = Post.find(params[:post_id])
     if @like.save
-      redirect_to user_post_path(params[:user_id], params[:id]), notice: 'Liked 👍'
+      redirect_to user_post_path(params[:user_id], params[:post_id]), notice: 'Liked 👍'
     else
-      redirect_to user_post_path(params[:user_id], params[:id]), alert: 'An error occured, please try again!'
+      redirect_to user_post_path(params[:user_id], params[:post_id]), alert: 'An error occured, please try again!'
     end
   end
 end
