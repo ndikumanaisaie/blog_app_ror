0871f7ee51780381d6a02aa64e1224a1345797d1
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-03-02 23:41:43 +0200

    Add redirect to user post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,10 +21,9 @@ class PostsController < ApplicationController
     end
     @post = Post.new(title: params[:post][:title], text: params[:post][:text], author_id: current_user.id)
     if @post.save
-      redirect_to post_path(@post)
+      redirect_to user_post_path(@post), notice: 'Post created successfully'
     else
-      flash[:error] = 'There is an error'
-      redirect_back(fallback_location: root_path)
+      render :new, alert: 'An error occured. Please try again!'
     end
   end
 end
