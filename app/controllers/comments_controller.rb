58b2928e22f0e02cfc70986094f390e209428659
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-03-02 23:57:08 +0200

    Change path

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,11 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(post_id: params[:id], author_id: current_user.id, text: params[:text])
     if @comment.save
-      @comment.update_comments_counter, notice: 'Comment created successfully'
+      @comment.update_comments_counter
+      flash[:success] = "Comment created successfully"
     else
       flash[:error] = 'An error has occurred, please try again later'
     end
-    redirect_to user_post_path(params[:user_id], params[:id])
+    redirect_to post_path(params[:user_id], params[:id])
   end
 end
