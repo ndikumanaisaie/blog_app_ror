79235c33eb650db623a064afc3e1378ec8353cf4
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-03-03 07:03:54 +0200

    Fix comment routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do 
     resources :posts, only: [:index, :show, :new, :create] do 
-      resources :comments, only: [:create]
-      resources :likes, only: [:index, :new, :create]
+      resources :comments, only: [:index, :new, :create]
+      resources :likes, only: [:create]
     end
   end
 end
