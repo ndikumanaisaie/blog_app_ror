6bbb72ebcfad9a5a5c7b2010c063baabd8c59f17
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-03-07 13:59:16 +0200

    Fix  seed error

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,20 +10,20 @@ Comment.destroy_all
 Post.destroy_all
 User.destroy_all
 
-User.create(name: 'Eliane', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Telecom Engineer at ARCT.')
-User.create(name: 'Charles', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-User.create(name: 'Simon', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Burundi.')
-User.create(name: 'Vianney', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Software Engineer.')
+user1 = User.create(name: 'Eliane', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Telecom Engineer at ARCT.')
+user2 = User.create(name: 'Charles', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+user3 = User.create(name: 'Simon', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Burundi.')
+user4 = User.create(name: 'Vianney', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Software Engineer.')
 
 
-Post.create(author_id: 1, title: 'Hello', text: 'This is my first post')
-Post.create(author_id: 2, title: 'Morning', text: 'This is my second post')
-Post.create(author_id: 1, title: 'God bless you', text: 'This is my Third post')
-Post.create(author_id: 1, title: 'Good night', text: 'This is my fourth post')
+post1 = Post.create(author: user1, title: 'Hello', text: 'This is my first post')
+post2 = Post.create(author: user1, title: 'Morning', text: 'This is my second post')
+post3 = Post.create(author: user2, title: 'God bless you', text: 'This is my Third post')
+post4 = Post.create(author: user2, title: 'Good night', text: 'This is my fourth post')
 
-Comment.create(post_id: 1, author_id: 1, text: 'wonderful day!' )
-Comment.create(post_id: 2, author_id: 2, text: 'You are amazing!')
-Comment.create(post_id: 2, author_id: 2, text: 'Nice sunday!')
-Comment.create(post_id: 2, author_id: 2, text: 'Microverse is super awosome')
-Comment.create(post_id: 2, author_id: 2, text: 'it is sunny')
-Comment.create(post_id: 2, author_id: 2, text: 'rails is magic')
+Comment.create(post: post1, author: user1, text: 'wonderful day!' )
+Comment.create(post: post1, author: user1, text: 'You are amazing!')
+Comment.create(post: post2, author: user1, text: 'Nice sunday!')
+Comment.create(post: post3, author: user2, text: 'Microverse is super awosome')
+Comment.create(post: post4, author: user2, text: 'it is sunny')
+Comment.create(post: post1, author: user1, text: 'rails is magic')
