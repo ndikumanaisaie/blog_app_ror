b2db64692f04cb3858fc9c08fa1fd415a6a4cc2c
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-03-09 21:23:39 +0200

    Add delete post and likes feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, ##:confirmable,
          :recoverable, :rememberable, :validatable
-  has_many :posts, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
   validates :post_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
